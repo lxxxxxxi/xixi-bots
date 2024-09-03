c1fdcf4a06a3e7bbb8049753af3968497179f218
lxiiiixi <104674636+lxiiiixi@users.noreply.github.com>
2024-09-03 13:09:20 +0800

    deploy

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
-import faston_bot from './faston_app.js';
-import kg_bot from './kgbot.js';
+import faston_bot from './xx_bot/xx.js';
+import kg_bot from './kg_bot/kgbot.js';
 
 const app = express();
 
@@ -11,14 +11,6 @@ app.get('/', (req, res) => {
 // app.get('/bot1', (req, res) => {
 //     res.status(200).send('Bot1 webhook processed');
 // });
-
-// // Webhook route for bot2
-// app.get('/bot2', (req, res) => {
-//     res.status(200).send('Bot2 webhook processed');
-// });
-
-// app.use(await bot.createWebhook({ domain: "https://xixi-bots.vercel.app/bot1" }));
-// app.use('/bot1', faston_bot.webhookCallback('/bot1'));
 
 const domain = 'https://xixi-bots.vercel.app';
 app.use(await faston_bot.createWebhook({ domain, path: '/bot1' }));
