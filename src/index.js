ac9cef2b6138bcd61480b3dc549cc92817466487
lxiiiixi <104674636+lxiiiixi@users.noreply.github.com>
2024-09-03 11:52:21 +0800

    deploy

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import express from 'express';
 
 const app = express();
 
-app.post('/bot1', (req, res) => {
+app.get('/bot1', (req, res) => {
     res.status(200).send('Bot1 webhook processed');
 });
 
 // Webhook route for bot2
-app.post('/bot2', (req, res) => {
+app.get('/bot2', (req, res) => {
     res.status(200).send('Bot2 webhook processed');
 });
 
